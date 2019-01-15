298806b40a23d554b60574ac3876ef5eb6415952
Alex <alexandersmith223@gmail.com>
2019-01-15 15:11:02 -0500

    Create OBV - On-balance volume
    - Add require 'obv' to technical_analysis
    - Create calculation
    - Create spec

--- lib/technical_analysis.rb
+++ lib/technical_analysis.rb
@@ -5,4 +5,4 @@ require 'technical_analysis/helpers/validation'
 # Indicators
 require 'technical_analysis/indicators/sma'
 require 'technical_analysis/indicators/adi'
-require 'technical_analysis/indicators/obv_mean'
+require 'technical_analysis/indicators/obv'
