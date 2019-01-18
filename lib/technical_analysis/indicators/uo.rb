bc1bc0bf01b92d6b41d3e95015b1a4042a64e01a
Alex <alexandersmith223@gmail.com>
2019-01-18 12:53:01 -0500

    Refactor calculation

--- lib/technical_analysis/indicators/uo.rb
+++ lib/technical_analysis/indicators/uo.rb
@@ -18,50 +18,42 @@ module TechnicalAnalysis
 
       data = data.sort_by_hash_date_asc # Sort data by descending dates
 
-      prior_closes = []
-      data.each { |v| prior_closes << v[:close] }
-
-      formatted_data = []
-      prior_closes.each_with_index do |prior_close, index|
-        v = data[index + 1]
-        unless v.nil?
-          min_low_p_close = [v[:low], prior_close].min
-          max_high_p_close = [v[:high], prior_close].max
-
-          buying_pressure = v[:close] - min_low_p_close
-          true_range = max_high_p_close - min_low_p_close
-
-          formatted_data << {
-            date: v[:date],
-            buying_pressure: buying_pressure,
-            true_range: true_range
-          }
-        end
-      end
-
-      buying_pressures = []
       output = []
+      period_values = []
+      prior_close = data.shift[:close]
       sum_of_weights = [short_weight, medium_weight, long_weight].sum
-      true_ranges = []
 
-      formatted_data.each do |v|
-        buying_pressures << v[:buying_pressure]
-        true_ranges << v[:true_range]
+      data.each do |v|
+        min_low_p_close = [v[:low], prior_close].min
+        max_high_p_close = [v[:high], prior_close].max
 
-        if buying_pressures.size == long_period && true_ranges.size == long_period
-          short_average = buying_pressures.last(short_period).sum / true_ranges.last(short_period).sum
-          medium_average = buying_pressures.last(medium_period).sum / true_ranges.last(medium_period).sum
-          long_average = buying_pressures.sum / true_ranges.sum
+        buying_pressure = v[:close] - min_low_p_close
+        true_range = max_high_p_close - min_low_p_close
 
+        period_values << { buying_pressure: buying_pressure, true_range: true_range }
+
+        if period_values.size == long_period
+          short_average = calculate_average(short_period, period_values)
+          medium_average = calculate_average(medium_period, period_values)
+          long_average = calculate_average(long_period, period_values)
           uo = 100 * (((short_weight * short_average) + (medium_weight * medium_average) + (long_weight * long_average)) / (sum_of_weights))
 
           output << { date: v[:date], value: uo }
-          buying_pressures.shift
-          true_ranges.shift
+
+          period_values.shift
         end
+
+        prior_close = v[:close]
       end
 
       output
+    end
+
+    def self.calculate_average(period, data)
+      buying_pressures_sum = data.last(period).map { |d| d[:buying_pressure] }.sum
+      true_ranges_sum = data.last(period).map { |d| d[:true_range] }.sum
+
+      buying_pressures_sum / true_ranges_sum
     end
 
   end
