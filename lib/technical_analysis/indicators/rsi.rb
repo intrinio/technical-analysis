6e59d63b6fd807d6bf0d15feba757d0d0f1f0255
Alex <alexandersmith223@gmail.com>
2019-01-18 15:57:12 -0500

    Refactor calculation and fix spec

--- lib/technical_analysis/indicators/rsi.rb
+++ lib/technical_analysis/indicators/rsi.rb
@@ -14,64 +14,50 @@ module TechnicalAnalysis
 
       data = data.sort_by_hash_date_asc # Sort data by descending dates
 
-      gains = []
-      avg_gains_and_losses = []
-      losses = []
       output = []
-      prev_price = data.first[price_key].to_f
+      prev_price = data.shift[price_key]
+      prev_avg = nil
       price_changes = []
       smoothing_period = period - 1
 
-      data.shift
-
-      # Create new avg_gains_and_losses array of hashes with keys :date, :avg_gain, and :avg_loss
-      # avg_gain - the average price gains over the last n periods, with smoothing
-      # avg_loss - the average price loss over the last n periods, with smoothing
       data.each do |v|
         price_change = (v[price_key] - prev_price)
         price_changes << price_change
 
-        if price_change >= 0
-          gains << price_change.abs
-          losses << 0.0
-        else
-          losses << price_change.abs
-          gains << 0.0
-        end
-
         if price_changes.size == period
-          if avg_gains_and_losses.empty?
-            avg_gain = gains.sum / period.to_f
-            avg_loss = losses.sum / period.to_f
+          if prev_avg.nil?
+            avg_gain = price_changes.map { |pc| pc.positive? ? pc : 0 }.sum / period.to_f
+            avg_loss = price_changes.map { |pc| pc.negative? ? pc.abs : 0 }.sum / period.to_f
           else
-            avg_gain = (((avg_gains_and_losses.last[:avg_gain] * smoothing_period) + gains.last) / period.to_f)
-            avg_loss = (((avg_gains_and_losses.last[:avg_loss] * smoothing_period) + losses.last) / period.to_f)
+            if price_change > 0
+              current_loss = 0
+              current_gain = price_change
+            elsif price_change < 0
+              current_loss = price_change.abs
+              current_gain = 0
+            else
+              current_loss = 0
+              current_gain = 0
+            end
+
+            avg_gain = (((prev_avg[:gain] * smoothing_period) + current_gain) / period.to_f)
+            avg_loss = (((prev_avg[:loss] * smoothing_period) + current_loss) / period.to_f)
           end
 
-          avg_gains_and_losses << { date: v[:date], avg_gain: avg_gain, avg_loss: avg_loss }
+          if avg_loss == 0
+            rsi = 100
+          else
+            rs = avg_gain / avg_loss
+            rsi = (100.00 - (100.00 / (1.00 + rs)))
+          end
 
+          output << { date: v[:date], value: rsi }
+
+          prev_avg = { gain: avg_gain, loss: avg_loss }
           price_changes.shift
-          gains.shift
-          losses.shift
         end
 
         prev_price = v[price_key]
-      end
-
-      # Use avg_gains_and_losses array of hashes to calculate relative strength (RS) and
-      # relative strength index (RSI) for output.
-      # Only need to calculate using the last n period.
-      avg_gains_and_losses = avg_gains_and_losses.last(period)
-
-      avg_gains_and_losses.each do |v|
-        if v[:avg_loss] == 0
-          rsi = 100
-        else
-          rs = (v[:avg_gain].to_f / v[:avg_loss].to_f)
-          rsi = (100.00 - (100.00 / (1.00 + rs)))
-        end
-
-        output << { date: v[:date], value: rsi }
       end
 
       output
