89082cf8f1fb7be3e41721515a8f1c99727d045c
Alex <alexandersmith223@gmail.com>
2019-01-18 16:09:25 -0500

    More trimming

--- lib/technical_analysis/indicators/sr.rb
+++ lib/technical_analysis/indicators/sr.rb
@@ -13,22 +13,21 @@ module TechnicalAnalysis
 
       data = data.sort_by_hash_date_asc # Sort data by descending dates
 
-      high_values = []
-      low_values = []
+      high_low_values = []
       output = []
       sr_values = []
 
       data.each do |v|
-        high_values << v[:high]
-        low_values << v[:low]
+        high_low_values << { high: v[:high], low: v[:low] }
 
-        if high_values.size == period && low_values.size == period
-          lowest_low = low_values.min
-          highest_high = high_values.max
+        if high_low_values.size == period
+          lowest_low = high_low_values.map { |hlv| hlv[:low] }.min
+          highest_high = high_low_values.map { |hlv| hlv[:high] }.max
+
           sr = (v[:close] - lowest_low) / (highest_high - lowest_low) * 100.00
 
           sr_values << sr
-      
+
           if sr_values.size == signal_period
             signal = sr_values.sum / signal_period.to_f
 
@@ -43,8 +42,7 @@ module TechnicalAnalysis
             sr_values.shift
           end
 
-          low_values.shift
-          high_values.shift
+          high_low_values.shift
         end
       end
 
