6ad807dfaf32b586a3ef14445030c6beeb8f98c6
Alex <alexandersmith223@gmail.com>
2019-01-22 14:08:23 -0500

    Add VPT - Volume-Price Trent
    - Add require 'vpt' to technical_analysis
    - Create calculation
    - Create spec

---
+++ lib/technical_analysis/indicators/vpt.rb
@@ -0,0 +1,31 @@
+module TechnicalAnalysis
+  class Vpt
+
+    # Calculates the volume-price trend (VPT) for the data over the given period
+    # https://en.wikipedia.org/wiki/Volume%E2%80%93price_trend
+    # 
+    # @param data [Array] Array of hashes with keys (:date, :close, :volume)
+    # @param period [Integer] The given period to calculate the VPT
+    # @return [Hash] A hash of the results with keys (:date, :value)
+    def self.calculate(data)
+      Validation.validate_numeric_data(data, :close, :volume)
+      Validation.validate_length(data, 2)
+
+      data = data.sort_by_hash_date_asc # Sort data by descending dates
+
+      output = []
+      prev_price = data.shift
+      prev_pvt = 0
+
+      data.each do |v|
+        pvt = prev_pvt + (((v[:close] - prev_price[:close]) / prev_price[:close]) * v[:volume])
+        output << { date: v[:date], value: pvt }
+        prev_price = v
+        prev_pvt = pvt
+      end
+
+      output
+    end
+
+  end
+end
