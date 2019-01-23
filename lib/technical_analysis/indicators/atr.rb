3187e63e3df91b667f8058fc91035e9619ffccf9
Alex <alexandersmith223@gmail.com>
2019-01-23 10:43:17 -0500

    Don't count first record
    - To make the true range calculation consistent with other indicators,
    don't count a true range for the first record.

--- lib/technical_analysis/indicators/atr.rb
+++ lib/technical_analysis/indicators/atr.rb
@@ -9,24 +9,20 @@ module TechnicalAnalysis
     # @return [Hash] A hash of the results with keys (:date, :value)
     def self.calculate(data, period: 14)
       Validation.validate_numeric_data(data, :high, :low, :close)
-      Validation.validate_length(data, period)
+      Validation.validate_length(data, period + 1)
 
       data = data.sort_by_hash_date_asc # Sort data by descending dates
 
       output = []
       period_values = []
-      prev_price = nil
+      prev_price = data.shift
 
       data.each do |v|
-        if prev_price.nil?
-          tr = v[:high] - v[:low]
-        else
-          tr = [
-            (v[:high] - v[:low]),
-            (v[:high] - prev_price[:close]).abs,
-            (v[:low] - prev_price[:close]).abs
-          ].max
-        end
+        tr = [
+          (v[:high] - v[:low]),
+          (v[:high] - prev_price[:close]).abs,
+          (v[:low] - prev_price[:close]).abs
+        ].max
 
         period_values << tr
 
