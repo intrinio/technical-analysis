6b6c2c6f58572a89b87108a6966b849fa849ebca
Alex <alexandersmith223@gmail.com>
2019-01-23 12:27:42 -0500

    Add BB - Bollinger Bands
    - Add require 'bb' to technical_analysis
    - Create calculation
    - Create spec
    - Add methods to array helper:
      - Mean (also aliased as average)
      - Sample variance
      - Standard deviation

--- lib/technical_analysis/helpers/array.rb
+++ lib/technical_analysis/helpers/array.rb
@@ -1,13 +1,31 @@
 class Array
+
   def sum
     self.inject(0, :+)
   end
 
+  def mean
+    self.sum / self.length.to_f
+  end
+
+  def sample_variance
+    m = self.mean
+    sum = self.inject(0) { |accum, i| accum + (i - m)**2 }
+    sum / (self.length - 1).to_f
+  end
+
+  def standard_deviation
+    Math.sqrt(self.sample_variance)
+  end
+
   def sort_by_hash_date_asc
-    self.sort_by {|row| row[:date]}
+    self.sort_by { |row| row[:date] }
   end
 
   def sort_by_hash_date_desc
     sort_by_hash_date_asc.reverse
   end
+  
+  alias_method :average, :mean
+
 end
