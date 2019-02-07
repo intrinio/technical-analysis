b6a79c32e8ce066a7031e2b7dca247d0012ebe05
Alex <alexandersmith223@gmail.com>
2019-02-07 17:12:45 -0500

    Add Yard comments/documentation & private methods

--- lib/technical_analysis/indicators/adx.rb
+++ lib/technical_analysis/indicators/adx.rb
@@ -1,22 +1,42 @@
 module TechnicalAnalysis
   class Adx < Indicator
 
+    # Returns the symbol of the technical indicator
+    #
+    # @return [String] A string of the symbol of the technical indicator
     def self.indicator_symbol
       "adx"
     end
 
+    # Returns the name of the technical indicator
+    #
+    # @return [String] A string of the name of the technical indicator
     def self.indicator_name
       "Average Directional Index"
     end
 
+    # Returns an array of valid keys for options for this technical indicator
+    #
+    # @return [Array] An array of keys as symbols for valid options for this technical indicator
     def self.valid_options
       %i(period)
     end
 
+    # Validates the provided options for this technical indicator
+    #
+    # @param options [Hash] The options for the technical indicator to be validated
+    #
+    # @return [Boolean] Returns true if options vare valid or raises an error if they're not
     def self.validate_options(options)
       Validation.validate_options(options, valid_options)
     end
 
+    # Calculates the minimum number of observations needed to calculate the technical indicator
+    #
+    # @param options [Hash] The options for the technical indicator
+    #
+    # @return [Integer] Returns the minimum number of observations needed to calculate the technical
+    #    indicator based on the options provided
     def self.min_data_size(period: 14)
       period.to_i * 2
     end
@@ -26,7 +46,29 @@ module TechnicalAnalysis
     #
     # @param data [Array] Array of hashes with keys (:date_time, :value)
     # @param period [Integer] The given period to calculate the ADX
-    # @return [Hash] A hash of the results with keys (:date_time, :value)
+    #
+    # @return [Array<Hash>]
+    #
+    #   An array of hashes with keys (:date_time, :value). Example output:
+    #
+    #     [
+    #       {
+    #         :date_time => "2019-01-09T00:00:00.000Z",
+    #         :value => {
+    #           :adx => 46.70506819299097,
+    #           :di_neg => 33.86727845364526,
+    #           :di_pos=>18.75156069669946
+    #         }
+    #       },
+    #       {
+    #         :date_time => "2019-01-08T00:00:00.000Z",
+    #         :value => {
+    #           :adx => 48.08801057392937,
+    #           :di_neg => 35.92768510004254,
+    #           :di_pos=>16.527665969119678
+    #         }
+    #       },
+    #     ]
     def self.calculate(data, period: 14)
       period = period.to_i
       Validation.validate_numeric_data(data, :high, :low, :close)
@@ -87,7 +129,7 @@ module TechnicalAnalysis
       output.sort_by_hash_date_time_desc
     end
 
-    def self.calculate_dm(current_price, prev_price)
+    private_class_method def self.calculate_dm(current_price, prev_price)
       if current_price[:high] - prev_price[:high] > prev_price[:low] - current_price[:low]
         dm_pos = [(current_price[:high] - prev_price[:high]), 0].max
         dm_neg = 0
@@ -102,7 +144,7 @@ module TechnicalAnalysis
       [dm_pos, dm_neg]
     end
   
-    def self.smooth_periodic_values(period, periodic_values, smoothed_values)
+    private_class_method def self.smooth_periodic_values(period, periodic_values, smoothed_values)
       if smoothed_values.empty?
         tr_period = periodic_values.map { |pv| pv[:tr] }.sum
         dm_pos_period = periodic_values.map { |pv| pv[:dm_pos] }.sum
