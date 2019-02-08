1b19e4b9d6d5e2b2df2ab63b7114ea1798050666
Alex <alexandersmith223@gmail.com>
2019-02-08 11:33:00 -0500

    Make helper methods private

--- lib/technical_analysis/indicators/ichimoku.rb
+++ lib/technical_analysis/indicators/ichimoku.rb
@@ -118,15 +118,15 @@ module TechnicalAnalysis
       output.sort_by_hash_date_time_desc
     end
 
-    def self.lowest_low(prices)
+    private_class_method def self.lowest_low(prices)
       prices.map { |price| price[:low] }.min
     end
 
-    def self.highest_high(prices)
+    private_class_method def self.highest_high(prices)
       prices.map { |price| price[:high] }.max
     end
     
-    def self.calculate_midpoint(index, period, data)
+    private_class_method def self.calculate_midpoint(index, period, data)
       period_range = ((index - (period - 1))..index)
       period_data = data[period_range]
       lowest_low = lowest_low(period_data)
@@ -135,7 +135,7 @@ module TechnicalAnalysis
       ((highest_high + lowest_low) / 2.0)
     end
 
-    def self.calculate_senkou_span_a(index, low_period, medium_period, data)
+    private_class_method def self.calculate_senkou_span_a(index, low_period, medium_period, data)
       mp_ago_index = (index - (medium_period - 1))
 
       tenkan_sen_mp_ago = calculate_midpoint(mp_ago_index, low_period, data)
@@ -144,13 +144,13 @@ module TechnicalAnalysis
       ((tenkan_sen_mp_ago + kinjun_sen_mp_ago) / 2.0)
     end
 
-    def self.calculate_senkou_span_b(index, medium_period, high_period, data)
+    private_class_method def self.calculate_senkou_span_b(index, medium_period, high_period, data)
       mp_ago_index = (index - (medium_period - 1))
 
       calculate_midpoint(mp_ago_index, high_period, data)
     end
 
-    def self.calculate_chikou_span(index, medium_period, data)
+    private_class_method def self.calculate_chikou_span(index, medium_period, data)
       mp_ago_index = (index - (medium_period - 1))
 
       data[mp_ago_index][:close]
